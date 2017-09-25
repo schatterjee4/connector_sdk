1cfd66d2191f7167b8c614d541f4294cc730d659
svchandra <chandra@workato.com>
2017-09-25 14:33:33 +0530

    Review Comments.
    Review comments.

--- api_key_auth/base.rb
+++ api_key_auth/base.rb
@@ -1,5 +1,5 @@
 {
-  title: "Base",
+  title: "Base CRM",
 
   connection: {
     fields: [
@@ -7,14 +7,14 @@
         name: "api_key",
         control_type: "password",
         optional: false,
-        label: "Personal Access Token"
+        label: "API key"
       }
     ],
     authorization: {
       type: "api_key",
 
       credentials: lambda do |connection|
-        headers("Authorization": "Bearer " +connection["api_key"])
+        headers("Authorization": "Bearer " + connection["api_key"])
       end
     }
   },
@@ -23,36 +23,33 @@
     lead: {
       fields: lambda do
         [
-          { name: "id", type: :integer, label: "Lead ID",
+          { name: "id", type: :integer, label: "Lead id",
             control_type: :number },
-          { name: "creator_id", type: :integer, label: "Created By(User ID)",
+          { name: "creator_id", type: :integer, label: "Created by(User ID)",
             control_type: :number },
-          { name: "owner_id", type: :integer, label: "Owner ID",
+          { name: "owner_id", type: :integer, label: "Owner id",
             control_type: :number },
-          { name: "first_name", type: :string, label: "First Name",
-            control_type: :text },
-          { name: "last_name", type: :string, label: "Last Name",
-            control_type: :text },
-          { name: "organization_name", type: :string,
-            label: "Organization Name", control_type: :text },
-          { name: "title", type: :string, control_type: :text },
-          { name: "description", type: :string, control_type: :text },
-          { name: "industry", type: :string, control_type: :text },
+          { name: "first_name"},
+          { name: "last_name" },
+          { name: "organization_name" },
+          { name: "title" },
+          { name: "description" },
+          { name: "industry" },
           { name: "website", type: :string, control_type: :url },
           { name: "email", type: :string, control_type: :email },
           { name: "phone", type: :string, control_type: :phone },
           { name: "mobile", type: :string, control_type: :phone },
           { name: "fax", type: :string, control_type: :phone },
-          { name: "twitter", type: :string, control_type: :text },
-          { name: "facebook", type: :string, control_type: :text },
-          { name: "linkedin", type: :string, control_type: :text },
-          { name: "skype", type: :string, control_type: :text },
+          { name: "twitter" },
+          { name: "facebook" },
+          { name: "linkedin" },
+          { name: "skype" },
           { name: "address", type: :object, properties: [
-              { name: "line1", type: :string, control_type: :text },
-              { name: "city", type: :string, control_type: :text },
-              { name: "postal_code", type: :string, control_type: :text },
-              { name: "state", type: :string, control_type: :text },
-              { name: "country", type: :string, control_type: :text }
+            { name: "line1" },
+            { name: "city" },
+            { name: "postal_code" },
+            { name: "state" },
+            { name: "country" }
           ]},
           { name: "created_at", type: :date_time, control_type: :timestamp },
           { name: "updated_at", type: :date_time, control_type: :timestamp }
@@ -69,51 +66,31 @@
   actions: {
     search_leads: {
       description: 'Search <span class="provider">Leads</span> in
-      	<span class="provider">BASE</span>',
-      subtitle: "Search leads in BASE",
+        <span class="provider">Base CRM</span>',
+      subtitle: "Search leads in Base CRM",
       help: "Search will only return leads matching all inputs",
-      input_fields: ->() {
+      input_fields: lambda do |object_definitions|
         [
           { name: "ids", type: :string, control_type: :text, label: "Id's",
             hint: "Comma-separated list of lead IDs." },
-          { name: "creator_id", type: :integer, control_type: :number,
-            label: "Created By(User ID)",
-            hint: "Returns all leads created by that user." },
-          { name: "owner_id", type: :integer, control_type: :number,
-            label: "Owner ID",
-            hint: "User ID. Returns all leads owned by that user." },
-          { name: "source_id", type: :integer, control_type: :number,
-            hint: "Id of the Source." },
-          { name: "first_name", type: :string, control_type: :text,
-            label: "First Name" },
-          { name: "last_name", type: :string, control_type: :text,
-            label: "Last Name" },
-          { name: "organization_name", type: :string, control_type: :text,
-            label: "Organization Name" },
-          { name: "status", type: :string, control_type: :text,
-            label: "Status of the lead" },
-          { name: "email", type: :string, control_type: :email,
-            label: "Email" },
-          { name: "phone", type: :string, control_type: :phone,
-            label: "Phone" },
-          { name: "mobile", type: :string, control_type: :phone,
-            label: "Mobile" },
           { name: "address[city]", type: :string, control_type: :text,
-            label: "City Name" },
+            label: "City name" },
           { name: "address[postal_code]", type: :string, control_type: :text,
-            label: "Zip/Postal Code" },
+            label: "Zip/postal code" },
           { name: "address[state]", type: :string, control_type: :text,
             label: "State/region name" },
           { name: "address[country]", type: :string, control_type: :text,
             label: "Country name" }
-        ]
-      },
+        ].concat(object_definitions["lead"].only("creator_id", "owner_id",
+          "source_id", "first_name", "last_name", "organization_name",
+          "status", "email", "phone", "mobile"))
+      end,
       execute: lambda do |connection, input|
         params = input.map do |key, value|
           "#{key}=#{value}"
         end.join("&")
         result = get("https://api.getbase.com/v2/leads?" + params)["items"]
-        leads = result.map { |lead| lead["data"] } unless result.blank?
+        leads = result.pluck("data")
         {
           leads: leads
         }
@@ -126,16 +103,15 @@
       },
       sample_output: lambda do
         {
-          leads: [
-            get("https://api.getbase.com/v2/leads")["items"].dig(0, "data")
-          ]
+          leads:
+          [ get("https://api.getbase.com/v2/leads")["items"].dig(0, "data") ]
         }
       end
     },
     create_lead: {
-      description: 'Create <span class="provider">Lead</span> in
-      	<span class="provider">BASE</span>',
-      subtitle: "Create lead in BASE",
+      description: 
+      'Create <span class="provider">Lead</span> in <span class="provider">Base CRM</span>',
+      subtitle: "Create lead in Base CRM",
       input_fields: lambda do |object_definitions|
         object_definitions["lead"].required("last_name", "organization_name").
           ignored("id", "creator_id","created_at", "updated_at", "owner_id")
@@ -157,8 +133,9 @@
       },
       sample_output: lambda do
         {
-          lead: get("https://api.getbase.com/v2/leads", per_page: 1)["items"].
-          dig(0, "data") || {}
+          lead:
+          get("https://api.getbase.com/v2/leads", per_page: 1)["items"].
+            dig(0, "data") || {}
         }
       end
     }
