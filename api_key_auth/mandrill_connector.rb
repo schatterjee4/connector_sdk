0681839501e9177235c07bfe71bf9188880b68f6
sharat-developer <sharat.develolper@gmail.com>
2017-09-28 15:16:27 +0530

    houndci-bot comment fix-2

--- api_key_auth/mandrill_connector.rb
+++ api_key_auth/mandrill_connector.rb
@@ -1,29 +1,29 @@
 {
-  title: 'Mandrill',
+  title: "Mandrill",
 
   connection: {
     fields: [
       {
-        name: 'api_key',
-        label: 'API Key',
-        control_type: 'password',
-        hint: 'You may find it ' \
+        name: "api_key",
+        label: "API Key",
+        control_type: "password",
+        hint: "You may find it " \
           "<a href='https://mandrillapp.com//settings'>here</a>",
         optional: false
       }
     ],
 
     authorization: {
-      type: 'api_key',
+      type: "api_key",
 
       credentials: lambda { |connection|
-        payload(key: connection['api_key'])
+        payload(key: connection["api_key"])
       }
     }
   },
 
   test: lambda { |_connection|
-    post('https://mandrillapp.com/api/1.0/templates/list.json')
+    post("https://mandrillapp.com/api/1.0/templates/list.json")
   },
 
   object_definitions: {
@@ -32,67 +32,67 @@
         template_variables = if config_fields.blank?
                                []
                              else
-                               post('https://mandrillapp.com/api/1.0/templates/info.json')
-                                 .payload(name: config_fields['template'])
-                                 .dig('code')
+                               post("https://mandrillapp.com/api/1.0/templates/info.json")
+                                 .payload(name: config_fields["template"])
+                                 .dig("code")
                                  .scan(/mc:edit=\"([^\"]*)\"/)
                                  .map do |var|
                                  {
                                    name: var.first,
-                                   hint: 'Include html tags for better formatting'
+                                   hint: "Include html tags for better formatting"
                                  }
                                end
           end
 
         [
           {
-            name: 'from_email',
-            hint: 'The default sender address for the template, if' \
-              ' provided - draft version',
+            name: "from_email",
+            hint: "The default sender address for the template, if" \
+              " provided - draft version",
             optional: false
           },
           {
-            name: 'from_name',
-            hint: 'The default sender from name for the template, if' \
-              ' provided - draft version'
+            name: "from_name",
+            hint: "The default sender from name for the template, if" \
+              " provided - draft version"
           },
           {
-            name: 'to',
-            hint: 'List of email recipients, one per line.',
+            name: "to",
+            hint: "List of email recipients, one per line.",
             optional: false
           },
           {
-            name: 'important',
-            hint: 'Whether or not this message is important, and should be' \
-              ' delivered ahead of non-important messages.',
-            control_type: 'checkbox',
-            type: 'boolean'
+            name: "important",
+            hint: "Whether or not this message is important, and should be" \
+              " delivered ahead of non-important messages.",
+            control_type: "checkbox",
+            type: "boolean"
           },
           {
-            name: 'track_opens',
-            hint: 'Whether or not to turn on open tracking for the message',
-            control_type: 'checkbox',
-            type: 'boolean'
+            name: "track_opens",
+            hint: "Whether or not to turn on open tracking for the message",
+            control_type: "checkbox",
+            type: "boolean"
           },
           {
-            name: 'track_clicks',
-            hint: 'Whether or not to turn on click tracking for the message',
-            control_type: 'checkbox',
-            type: 'boolean'
+            name: "track_clicks",
+            hint: "Whether or not to turn on click tracking for the message",
+            control_type: "checkbox",
+            type: "boolean"
           },
           {
-            name: 'send_at',
-            hint: 'When this message should be sent as a UTC timestamp in' \
-              ' YYYY-MM-DD HH:MM:SS format. If you specify a time in the'  \
-              ' past, the message will be sent immediately. Example: 2017-09-28 09:00:00'
+            name: "send_at",
+            hint: "When this message should be sent as a UTC timestamp in" \
+              " YYYY-MM-DD HH:MM:SS format. If you specify a time in the"  \
+              " past, the message will be sent immediately. Example: 2017-09-28 09:00:00"
           }
         ].concat(if template_variables.blank?
                    []
                  else
                    [
                      {
-                       name: 'template_content',
-                       type: 'object',
+                       name: "template_content",
+                       type: "object",
                        properties: template_variables
                      }
                    ]
@@ -108,53 +108,53 @@
 
       config_fields: [
         {
-          name: 'template',
-          control_type: 'select',
-          pick_list: 'templates',
+          name: "template",
+          control_type: "select",
+          pick_list: "templates",
           optional: false
         }
       ],
 
       input_fields: lambda { |object_definitions|
-        object_definitions['send_template_input']
+        object_definitions["send_template_input"]
       },
 
       execute: lambda { |_connection, input|
         message = {
-          from_email: input['from_email'],
-          from_name: input['from_name'],
-          to: input['to']
+          from_email: input["from_email"],
+          from_name: input["from_name"],
+          to: input["to"]
                   .split("\n")
                   .map { |to| { email: to.strip } },
-          important: input['important'],
-          track_opens: input['track_opens'],
-          track_clicks: input['track_clicks']
+          important: input["important"],
+          track_opens: input["track_opens"],
+          track_clicks: input["track_clicks"]
         }
 
-        post('https://mandrillapp.com/api/1.0/messages/send-template.json')
-          .payload(template_name: input['template'],
-                   template_content: (input['template_content'] || [])
+        post("https://mandrillapp.com/api/1.0/messages/send-template.json")
+          .payload(template_name: input["template"],
+                   template_content: (input["template_content"] || [])
                     .map { |key, val| { name: key, content: val } },
                    message:   message,
-                   send_at: input['send_at'])
+                   send_at: input["send_at"])
           &.first
       },
 
       output_fields: lambda { |_object_definitions|
         [
-          { name: 'email' },
-          { name: 'status' },
-          { name: '_id' },
-          { name: 'reject_reason' }
+          { name: "email" },
+          { name: "status" },
+          { name: "_id" },
+          { name: "reject_reason" }
         ]
       },
 
       sample_output: lambda {
         {
-          email: 'mail@workato.com',
-          status: 'send',
-          _id: 'abc123abc123abc123abc123abc123',
-          reject_reason: 'hard-bounce'
+          email: "mail@workato.com",
+          status: "send",
+          _id: "abc123abc123abc123abc123abc123",
+          reject_reason: "hard-bounce"
         }
       }
     }
@@ -162,8 +162,8 @@
 
   pick_lists: {
     templates: lambda { |_connection|
-      post('https://mandrillapp.com/api/1.0/templates/list.json')
-        .map { |template| [template['name'], template['slug']] }
+      post("https://mandrillapp.com/api/1.0/templates/list.json")
+        .map { |template| [template["name"], template["slug"]] }
     }
   }
 }
