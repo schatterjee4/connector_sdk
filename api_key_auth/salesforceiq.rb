f6cc994af907406dbaf3f99c1c455505819ed9e5
svchandra <chandra@workato.com>
2017-09-29 13:41:59 +0530

    Hound and Review comments.

--- api_key_auth/salesforceiq.rb
+++ api_key_auth/salesforceiq.rb
@@ -27,8 +27,8 @@
           { name: "id" },
           { name: "name", optional: false },
           { name: "modifiedDate", label: "Modified date", type: :integer,
-            hint:
-            "Stores a particular Date & Time in UTC milliseconds past the epoch." },# milliseconds since epoch
+            hint: "Stores a particular Date & Time in UTC milliseconds past" \
+             " the epoch." },# milliseconds since epoch
         ].concat(
           get("https://api.salesforceiq.com/v2/accounts/fields")["fields"].
           map do |field|
@@ -47,14 +47,13 @@
 
   actions: {
     create_account: {
-      description:
-      "Create <span class='provider'>Account</span> in " \
+      description: "Create <span class='provider'>Account</span> in " \
       "<span class='provider'>SalesforceIQ</span>",
       input_fields: lambda do |object_definitions|
         object_definitions["account"].ignored("id", "modifiedDate", "address_city", 
         "address_state", "address_postal_code", "address_country")
       end,
-      execute: lambda do |connection,input|
+      execute: lambda do |connection, input|
         fields = {}
         input.each do |k, v|
           if k != "name"
@@ -78,12 +77,13 @@
        " accounts, if blank.",
       input_fields: lambda do
         [
-          { name: "_ids", label: "Account identifiers",
-            hint: "Comma separated list of Account identifiers" 
+          {
+            name: "_ids", label: "Account identifiers",
+            hint: "Comma separated list of Account identifiers"
           }
         ]
       end,
-      execute: lambda do |connection,input|
+      execute: lambda do |connection, input|
         accounts = get("https://api.salesforceiq.com/v2/accounts",
           input)["objects"].
           each do |account| # add each custom field to account response object
@@ -126,29 +126,24 @@
         ]
       end,
       poll: lambda do |connection, input, modified_date_since|
-        modified_date = modified_date_since || ((input["since"].presence || 
-          Time.now).to_time.to_f * 1000).to_i
+        limit = 50
+        modified_date ||= ((input["since"].presence || Time.now).
+          to_time.to_f * 1000).to_i
         result = get("https://api.salesforceiq.com/v2/accounts").
-          params(_limit: 50, _start: 0,
+          params(_limit: limit, _start: 0,
           modifiedDate: modified_date)["objects"] # result returns in ascending order
         accounts = result.each do |account|
           (account["fieldValues"] || {}).map do |k, v|
             account[k] = v.first["raw"]
           end
         end
-        if accounts.size == 0
-          modified_date_since = (Time.now.to_f * 1000).to_i
-        else
-          modified_date_since = accounts.last["modifiedDate"]
-        end
+        modified_date_since = accounts.dig(-1, "modifiedDate") ||
+         (Time.now.to_f * 1000).to_i
         {
           events: accounts,
           next_poll: modified_date_since,
-          next_page: accounts.size == 50
+          can_poll_more: accounts.size >= limit
         }
-      end,
-      sort_by: lambda do |account|
-        account["modifiedDate"]
       end,
       dedup: lambda do |account|
         [account["id"], account["modifiedDate"]].join("_")
@@ -157,7 +152,8 @@
         object_definitions["account"]
       end,
       sample_output: lambda do
-        get("https://api.salesforceiq.com/v2/accounts")["objects"]&.first || {}
+        get("https://api.salesforceiq.com/v2/accounts").
+          params(_limit: 1).dig("objects", 0)
       end
     }
   }
