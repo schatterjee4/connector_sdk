02c1eab2b5bdcdc518f22a47ad853b88b9890053
brendonyang <39894932+brendonyang@users.noreply.github.com>
2018-06-26 14:12:26 +0800

    Calendly Connector SDK - fixed hound comments v7
    added spaces after colon in sample output

--- api_key_auth/Calendly.rb
+++ api_key_auth/Calendly.rb
@@ -300,111 +300,111 @@
 
       sample_output: lambda do |_connection, input|
         {
-          "event":"invitee.created",
-          "time":"2018-03-14T19:16:01Z",
-          "payload":{
-            "event_type":{
-              "uuid":"CCCCCCCCCCCCCCCC",
-              "kind":"One-on-One",
-              "slug":"event_type_name",
-              "name":"Event Type Name",
-              "duration":15,
-              "owner":{
-                "type":"users",
-                "uuid":"DDDDDDDDDDDDDDDD"
+          "event": "invitee.created",
+          "time": "2018-03-14T19:16:01Z",
+          "payload": {
+            "event_type": {
+              "uuid": "CCCCCCCCCCCCCCCC",
+              "kind": "One-on-One",
+              "slug": "event_type_name",
+              "name": "Event Type Name",
+              "duration": 15,
+              "owner": {
+                "type": "users",
+                "uuid": "DDDDDDDDDDDDDDDD"
               }
             },
-            "event":{
-              "uuid":"BBBBBBBBBBBBBBBB",
-              "assigned_to":[
+            "event": {
+              "uuid": "BBBBBBBBBBBBBBBB",
+              "assigned_to": [
                 "Jane Sample Data"
               ],
-              "extended_assigned_to":[
+              "extended_assigned_to": [
                 {
-                  "name":"Jane Sample Data",
-                  "email":"user@example.com",
-                  "primary":false
+                  "name": "Jane Sample Data",
+                  "email": "user@example.com",
+                  "primary": false
                 }
               ],
-              "start_time":"2018-03-14T12:00:00Z",
-              "start_time_pretty":"12:00pm - Wednesday, March 14, 2018",
-              "invitee_start_time":"2018-03-14T12:00:00Z",
-              "invitee_start_time_pretty":"12:00pm - Wednesday, March 14, 2018",
-              "end_time":"2018-03-14T12:15:00Z",
-              "end_time_pretty":"12:15pm - Wednesday, March 14, 2018",
-              "invitee_end_time":"2018-03-14T12:15:00Z",
-              "invitee_end_time_pretty":"12:15pm - Wednesday, March 14, 2018",
-              "created_at":"2018-03-14T00:00:00Z",
-              "location":"The Coffee Shop",
-              "canceled":false,
-              "canceler_name":"",
-              "cancel_reason":"",
-              "canceled_at":""
+              "start_time": "2018-03-14T12:00:00Z",
+              "start_time_pretty": "12:00pm - Wednesday, March 14, 2018",
+              "invitee_start_time": "2018-03-14T12:00:00Z",
+              "invitee_start_time_pretty": "12:00pm - Wednesday, March 14, 2018",
+              "end_time": "2018-03-14T12:15:00Z",
+              "end_time_pretty": "12:15pm - Wednesday, March 14, 2018",
+              "invitee_end_time": "2018-03-14T12:15:00Z",
+              "invitee_end_time_pretty": "12:15pm - Wednesday, March 14, 2018",
+              "created_at": "2018-03-14T00:00:00Z",
+              "location": "The Coffee Shop",
+              "canceled": false,
+              "canceler_name": "",
+              "cancel_reason": "",
+              "canceled_at": ""
             },
-            "invitee":{
-              "uuid":"AAAAAAAAAAAAAAAA",
-              "first_name":"Joe",
-              "last_name":"Sample Data",
-              "name":"Joe Sample Data",
-              "email":"not.a.real.email@example.com",
-              "timezone":"UTC",
-              "created_at":"2018-03-14T00:00:00Z",
-              "is_reschedule":false,
+            "invitee": {
+              "uuid": "AAAAAAAAAAAAAAAA",
+              "first_name": "Joe",
+              "last_name": "Sample Data",
+              "name": "Joe Sample Data",
+              "email": "not.a.real.email@example.com",
+              "timezone": "UTC",
+              "created_at": "2018-03-14T00:00:00Z",
+              "is_reschedule": false,
               "payments":[
                 {
-                  "id":"ch_AAAAAAAAAAAAAAAAAAAAAAAA",
-                  "provider":"stripe",
-                  "amount":1234.56,
-                  "currency":"USD",
-                  "terms":"sample terms of payment (up to 1,024 characters)",
-                  "successful":true
+                  "id": "ch_AAAAAAAAAAAAAAAAAAAAAAAA",
+                  "provider": "stripe",
+                  "amount": 1234.56,
+                  "currency": "USD",
+                  "terms": "sample terms of payment (up to 1,024 characters)",
+                  "successful": true
                   }
               ],
-              "canceled":false,
-              "canceler_name":"",
-              "cancel_reason":"",
-              "canceled_at":""
+              "canceled": false,
+              "canceler_name": "",
+              "cancel_reason": "",
+              "canceled_at": ""
             },
-            "questions_and_answers":[
+            "questions_and_answers": [
               {
-                "question":"Skype ID",
-                "answer":"fake_skype_id"
+                "question": "Skype ID",
+                "answer": "fake_skype_id"
               },
               {
-                "question":"Facebook ID",
-                "answer":"fake_facebook_id"
+                "question": "Facebook ID",
+                "answer": "fake_facebook_id"
               },
               {
-                "question":"Twitter ID",
-                "answer":"fake_twitter_id"
+                "question": "Twitter ID",
+                "answer": "fake_twitter_id"
               },
               {
-                "question":"Google ID",
-                "answer":"fake_google_id"
+                "question": "Google ID",
+                "answer": "fake_google_id"
               }
             ],
-            "questions_and_responses":{
-              "1_question":"Skype ID",
-              "1_response":"fake_skype_id",
-              "2_question":"Facebook ID",
-              "2_response":"fake_facebook_id",
-              "3_question":"Twitter ID",
-              "3_response":"fake_twitter_id",
-              "4_question":"Google ID",
-              "4_response":"fake_google_id"
+            "questions_and_responses": {
+              "1_question": "Skype ID",
+              "1_response": "fake_skype_id",
+              "2_question": "Facebook ID",
+              "2_response": "fake_facebook_id",
+              "3_question": "Twitter ID",
+              "3_response": "fake_twitter_id",
+              "4_question": "Google ID",
+              "4_response": "fake_google_id"
             },
-            "tracking":{
-              "utm_campaign":"",
-              "utm_source":"",
-              "utm_medium":"",
-              "utm_content":"",
-              "utm_term":"",
-              "salesforce_uuid":""
+            "tracking": {
+              "utm_campaign": "",
+              "utm_source": "",
+              "utm_medium": "",
+              "utm_content": "",
+              "utm_term": "",
+              "salesforce_uuid": ""
             },
-            "old_event":"",
-            "old_invitee":"",
-            "new_event":"",
-            "new_invitee":""
+            "old_event": "",
+            "old_invitee": "",
+            "new_event": "",
+            "new_invitee": ""
           }
         }
       end
