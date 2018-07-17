bd68248f019c6398a862236b935b2ecc7801de73
ctjsctjs <ctjsctjs@gmail.com>
2018-07-17 16:57:31 +0800

    Updated User Interface
    Moved adapter section up
    Added navigation links
    Code Cleaning
    Added responsive Nav
    Added fade animation for search filter

--- js/script.js
+++ js/script.js
@@ -1,30 +1,58 @@
+//On Init
+$( document ).ready(function() {
+  generateAdapters();
+});
+
 //Search Function
 function filterFunc(event) {
   var userInput = event.value.toUpperCase();
   $('.app').each(function() {
     if (this.textContent.toUpperCase().indexOf(userInput) > -1) {
-      this.style.display = "";
+      $(this).fadeIn( "fast" , "swing");
     } else {
-      this.style.display = "none";
+      $(this).fadeOut( "fast", "swing" );
     }
   });
 }
 
 //Load JSON file to generate app listing
-$.getJSON("adapters/adapters.json", function (json) {
-  var container = $("#container-apps");
-  var listCol = 5;
+function generateAdapters(){
 
-  //Generate app list from adapters.JSON
-  json.adapters.forEach(function (adapter) {
-    var li = $('<li class="app">').appendTo(container);
-    var a = $('<a target="_blank">').attr('href', adapter.link).appendTo(li);
-    var img = $('<img>').attr('src', adapter.image).appendTo(a);
-    $('<div class="item-name black-font"></div>').text(adapter.name).appendTo(li);
+  //Load JSON file
+  $.getJSON("adapters/adapters.json", function (json) {
+    var listCol = 5;
+    var container = $("#container-apps");
+
+    //Generate app list from adapters.JSON
+    json.adapters.forEach(function (adapter) {
+
+      var link = adapter.link;
+      var img = adapter.image;
+      var name = adapter.name;
+
+      var content = '<li class="app">' +
+      '<a target="_blank" href="' + link + '">' +
+      '<img src="' + img + '">' +
+      '<div class="item-name black-font">' + name + '</div>'
+
+      $(content).appendTo(container);
+    });
+
+    //Generate dummy box for responsive
+    for (i = 0; i < (listCol); i++) {
+      $('<li class="item flex-dummy"></li>').appendTo(container);
+    }
   });
+}
 
-  //Generate dummy box for responsive
-  for (i = 0; i < (listCol); i++) {
-    $('<li class="item flex-dummy"></li>').appendTo(container);
-  }
-});
+//Function to toggle visibility of navigation dropdown for mobile nav
+function toggleSideNav() {
+  console.log('clicked');
+    if ($( '.nav-links-li' ).is( ":hidden" )){
+      $('.nav-links-li').removeClass('hide');
+      $('.nav-links-li').addClass('show');
+    } else {
+      $('.nav-links-li').removeClass('show');
+      $('.nav-links-li').addClass('hide');
+    }
+}
